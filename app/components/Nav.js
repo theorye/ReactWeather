4463dcc251ed9f3f17ec04af37645c43469912bf
Adrian Haugsness <adrianhaugsness@gmail.com>
2016-06-12 08:25:32 -0500

    added styles

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,6 +1,53 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
 
+const Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+
+    alert('Not yet wired up');
+  },
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to='/examples' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+})
+
+export default Nav;
+
+// <h2>nav component</h2>
+// <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+// <Link to='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+// <Link to='/examples' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+
 // var Nav = React.createClass({
 //   render: function() {
 //     return (
@@ -13,16 +60,3 @@ import { Link, IndexLink } from 'react-router';
 //     );
 //   }
 // });
-
-const Nav = (props) => {
-  return (
-    <div>
-      <h2>nav component</h2>
-      <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to='/examples' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>
-  );
-}
-
-export default Nav;
