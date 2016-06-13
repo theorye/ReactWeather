77449252970c49a4c5e9d01a8bc4b0ffd84e7f39
Adrian Haugsness <adrianhaugsness@gmail.com>
2016-06-13 07:35:44 -0500

    Fixed modal issue

--- app/components/ErrorModal.js
+++ app/components/ErrorModal.js
@@ -25,11 +25,19 @@ const ErrorModal = React.createClass({
         </p>
       </div>
     );
+    // Remove node if there is One
+    $(".reveal-overlay").remove();
 
-    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    // Build HTML and render to string
+    let $modal = $(ReactDOMServer.renderToString(modalMarkup));
+
+    // Attach to DOMnode
     $(ReactDOM.findDOMNode(this)).html($modal);
 
+    //Create a new instace of a modal
     const modal = new Foundation.Reveal($('#error-modal'));
+
+    // Open Modal
     modal.open();
   },
   render: function() {
